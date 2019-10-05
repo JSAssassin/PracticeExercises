0827280e539a24c32bc7ad203b0a3e96618c0662
Nish Tahir <nishtahir@outlook.com>
2019-10-05 00:28:34 -0400

    Update crazyCaps.js
    Fixed formatting

--- crazyCaps.js
+++ crazyCaps.js
@@ -1,18 +1,17 @@
-/* Define a function, crazyCaps, that accepts a string as an argument. 
-crazyCaps should return a string in which every other character is capitalized. 
-The first letter should be lower-cased.*/
-
-
+/**
+ * Define a function, crazyCaps, that accepts a string as an argument. 
+ * crazyCaps should return a string in which every other character is capitalized. 
+ * The first letter should be lower-cased.
+*/
 function crazyCaps(a) {
   let result = "";
-  for(let i=0; i<a.length; i++){
+  for(let i = 0; i < a.length; i++) {
       if(i % 2 === 0){
         result += a[i].toLowerCase();
       } else {
         result += a[i].toUpperCase();
       }
   }
-
   return result;
 }
 
